fd344f64085a546d5edd64046460714b60b2d4e4
Sören Brockmann <Me@Ni-hao.fritz.box>
2017-11-04 21:29:36 +0100

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -17,16 +17,22 @@ const users = [{
   _id: userTwoId,
   email: 'soeren2@vifavi.com',
   password: 'userTwoPass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+  }]
 }]
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'first test todo'
+  text: 'first test todo',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second test todo',
   completed: true,
-  completedAt: 333
+  completedAt: 333,
+  _creator: userTwoId
 }]
 
 const populateTodos = (done) => {
